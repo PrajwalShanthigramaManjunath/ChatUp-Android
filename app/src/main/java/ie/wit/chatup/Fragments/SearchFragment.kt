c06e6a536eca00dde8fff080100b286715491779
Prajwal <prajwal.shanthigrama@gmail.com>
2020-04-27 23:08:11 +0100

    .

--- app/src/main/java/ie/wit/chatup/Fragments/SearchFragment.kt
+++ app/src/main/java/ie/wit/chatup/Fragments/SearchFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import ie.wit.chatup.AdapterClasses.UserAdapter
 import ie.wit.chatup.ModelClasses.Users
 import ie.wit.chatup.R
@@ -20,6 +22,14 @@ private var userAdapter: UserAdapter? = null
         ): View? {
             // Inflate the layout for this fragment
             val view: View = inflater.inflate(R.layout.fragment_search, container, false)
+            mUsers = ArrayList()
+            retrieveAllUsers()
             return view
         }
-}
+
+        private fun retrieveAllUsers() {
+            var firebaseUserID = FirebaseAuth.getInstance().currentUser!!.uid
+            val refUsers = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUserID)
+
+        }
+    }
