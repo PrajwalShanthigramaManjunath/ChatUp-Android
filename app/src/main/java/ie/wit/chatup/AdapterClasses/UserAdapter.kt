0248887fd63454838090c1a4f5f4fb406a7a5382
Prajwal <prajwal.shanthigrama@gmail.com>
2020-04-27 22:12:37 +0100

    search user

--- app/src/main/java/ie/wit/chatup/AdapterClasses/UserAdapter.kt
+++ app/src/main/java/ie/wit/chatup/AdapterClasses/UserAdapter.kt
@@ -2,8 +2,11 @@ package ie.wit.chatup.AdapterClasses
 
 import android.content.Context
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import de.hdodenhof.circleimageview.CircleImageView
 import ie.wit.chatup.ModelClasses.Users
+import ie.wit.chatup.R
 
 class UserAdapter (
     mContext: Context,
@@ -14,10 +17,17 @@ class UserAdapter (
         class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
         {
             var userNameTxt: TextView
-            var userNameTxt: TextView
-            var userNameTxt: TextView
-            var userNameTxt: TextView
-            var userNameTxt: TextView
+            var profileImageView: CircleImageView
+            var onlineTextView: TextView                              // online text view
+            var ofllineTextView: TextView                            // offline text view
+            var lastMessageTxt: TextView
+init{
+    userNameTxt = itemView.findViewById(R.id.username)
+    userNameTxt = itemView.findViewById(R.id.username)
+    userNameTxt = itemView.findViewById(R.id.username)
+    userNameTxt = itemView.findViewById(R.id.username)
+    userNameTxt = itemView.findViewById(R.id.username)
 
+}
         }
     }
