b3a241bddb55caa13a13267a49eebe6b16359da2
Prajwal <prajwal.shanthigrama@gmail.com>
2020-05-03 22:47:38 +0100

    send text in message, retrieve

--- app/src/main/java/ie/wit/chatup/AdapterClasses/UserAdapter.kt
+++ app/src/main/java/ie/wit/chatup/AdapterClasses/UserAdapter.kt
@@ -1,13 +1,17 @@
 package ie.wit.chatup.AdapterClasses
 
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
+import ie.wit.chatup.MessageChatActivity
 import ie.wit.chatup.ModelClasses.Users
 import ie.wit.chatup.R
 
@@ -41,7 +45,30 @@ return mUsers.size
             val user: Users = mUsers[position]
             holder.userNameTxt.text = user!!.getUserName()
             Picasso.get().load(user.getProfile()).placeholder(R.drawable.profile).into(holder.profileImageView)
+
+            holder.itemView.setOnClickListener {
+                val options = arrayOf<CharSequence>(
+                    "Send Message",
+                    "Visit Profile"
+                )
+                val builder: AlertDialog.Builder = AlertDialog.Builder(mContext)
+                builder.setTitle("What do you want?")
+                builder.setItems(options, DialogInterface.OnClickListener { dialog, position ->
+                    if (position == 0)
+                    {
+                        val intent = Intent(mContext, MessageChatActivity::class.java)
+                        intent.putExtra("visit_id", user.getUID())
+                        mContext.startActivity(intent)
+                    }
+                    if (position == 1)
+                    {
+
+                    }
+                })
+
+            }
         }
+
 
         class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
         {
--- app/src/main/java/ie/wit/chatup/MessageChatActivity.kt
+++ app/src/main/java/ie/wit/chatup/MessageChatActivity.kt
@@ -1,26 +1,83 @@
 package ie.wit.chatup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_message_chat.*
 
 class MessageChatActivity : AppCompatActivity() {
+    var userIdVisit: String = ""
+    var firebaseUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message_chat)
 
+        intent = intent
+        userIdVisit = intent.getStringExtra("visit_id")
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        val reference = FirebaseDatabase.getInstance().reference.child("Users")
+            .child(userIdVisit)
+        reference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
         send_message_btn.setOnClickListener {
             val message = text_message.text.toString()
-            if (message == "")
-            {
-                Toast.makeText(this@MessageChatActivity, "Please write a message..", Toast.LENGTH_LONG).show()
-            }
-            else
-            {
-                sendMessageToUser()
+            if (message == "") {
+                Toast.makeText(
+                    this@MessageChatActivity,
+                    "Please write a message..",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                sendMessageToUser(firebaseUser!!.uid, userIdVisit, message)
             }
         }
+    }
+
+    private fun sendMessageToUser(senderId: String, receiverId: String?, message: String) {
+        val reference = FirebaseDatabase.getInstance().reference
+        val messageKey = reference.push().key
+
+
+        val messageHashMap = HashMap<String, Any?>()
+        messageHashMap["sender"] = senderId
+        messageHashMap["message"] = message
+        messageHashMap["receiver"] = receiverId
+        messageHashMap["isseen"] = false
+        messageHashMap["url"] = ""
+        messageHashMap["messageId"] = messageKey
+        reference.child("Chats")
+            .child(messageKey!!)
+            .setValue(messageHashMap)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val chatsListReference = FirebaseDatabase.getInstance()
+                        .reference
+                        .child("ChatList")
+
+                    //need to implement push notification using fcm
+                    chatsListReference.child("id").setValue(firebaseUser!!.uid)
+                    val reference = FirebaseDatabase.getInstance().reference.child("Users")
+                        .child(firebaseUser!!.uid)
+
+
+                }
+            }
     }
 }
